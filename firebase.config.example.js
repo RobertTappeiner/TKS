391b44378f9c7c60e716be979e6cd0fc3782453a
RobertTappeiner <r.tappeiner84@gmail.com>
2026-02-24 15:34:30 +0100

    firebase

---
+++ firebase.config.example.js
@@ -0,0 +1,10 @@
+window.TKS_FIREBASE_CONFIG = {
+    apiKey: "YOUR_FIREBASE_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+window.TKS_FIREBASE_STORAGE_PATH = "contact_uploads";
